c9c4ce6bb8ee2a8b5826e60202378bfd874ac8fd
PopckovS <popckovM10@yandex.ru>
2022-11-24 16:43:23 +0300

    Добавил работу с переменными окружения, рефакторинг кода.

--- programs_1/file_crypt/__main__.py
+++ programs_1/file_crypt/__main__.py
@@ -5,19 +5,24 @@ from pathlib import Path
 
 import pyAesCrypt as pac
 
-from settings import MIN_PASSWORD_LEN, BUFFER_SIZE, PASSWORD
+from settings import MIN_PASSWORD_LEN, BUFFER_SIZE, PASSWORD, EXTENSION
+from typing import Union
 
 
-def encrypt_file(file_path: Path, password: str, ignor_error=True):
+def encrypt_file(
+        file_path: Union[Path, str],
+        password: str,
+        extension: str,
+        ignor_error: bool = True) -> Path:
     """
-    Encrypt file
+    Encrypt file with password
 
-    :param file_path:
-    :param password:
-    :param ignor_error:
+    :param file_path: path to the file
+    :param password: password for encrypt file
+    :param extension: file extension for encrypt
+    :param ignor_error: ignor error or not
     :return:
     """
-
     if not isinstance(password, str) or len(password) < MIN_PASSWORD_LEN:
         logging.error("Password not not valid or too short. Min password %s", MIN_PASSWORD_LEN)
         return
@@ -29,13 +34,12 @@ def encrypt_file(file_path: Path, password: str, ignor_error=True):
     if not isinstance(file_path, Path):
         file_path = Path(file_path)
 
-    new_path = file_path.as_posix().with_suffix('.aes')
-
+    # encrypt file
+    new_path = file_path.as_posix().with_suffix(extension)
     pac.encryptFile(file_path,
                     new_path,
                     password,
-                    BUFFER_SIZE
-                    )
+                    BUFFER_SIZE)
     return Path(new_path)
 
 
@@ -43,7 +47,14 @@ def decrypt_file():
     pass
 
 
-def dirs_travel_encrypt(path_to_dir, password):
+def dirs_travel_encrypt(path_to_dir: Union[Path, str], password: str) -> list:
+    """
+    Walk through directory and encrypts all inner file
+
+    :param path_to_dir:
+    :param password:
+    :return: list of path to encrypt file
+    """
     files_struct = []
     for root, dirs, files in os.walk(path_to_dir):
         for file in files:
@@ -54,28 +65,32 @@ def dirs_travel_encrypt(path_to_dir, password):
     pass
 
 
-if __name__ == "__main__":
+def get_args() -> argparse:
+    """
+    Returned parameter of terminal arguments
+
+    :return: argparse object
+    """
     parser = argparse.ArgumentParser(description='File encryption and decryption service')
 
-    parser.add_argument('--path', type=str, required=False, help='The path to the folder to be encrypted')
-    parser.add_argument('--password', type=str, required=False, help='The password for encrypt')
-    _args = parser.parse_args()
+    parser.add_argument('-ex', type=str, required=False, help='Extension for encrypt file')
+    parser.add_argument('-password', type=str, required=False, help='The password for encrypt')
+    parser.add_argument('-path', type=str, required=False, help='The path to the folder to be encrypted')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    _args = get_args()
 
     if _args.path:
         path = Path(_args.path)
+        extension = _args.ex if hasattr(_args, "ex") else EXTENSION
+        password = _args.password if hasattr(_args, "password") else PASSWORD
 
         if path.is_dir():
             dirs_travel_encrypt(
-                path.as_posix(),
-                _args.password if hasattr(_args, "password") else PASSWORD
-            )
+                path.as_posix(), password, extension)
 
         if path.is_file():
             encrypt_file(
-                path.as_posix(),
-                _args.password if hasattr(_args, "password") else PASSWORD
-            )
-
-
-
-
+                path.as_posix(), password, extension)
--- programs_1/file_crypt/settings.py
+++ programs_1/file_crypt/settings.py
@@ -1,11 +1,13 @@
 from os import environ
+from dotenv import load_dotenv
 
-# min password length
+load_dotenv()
+
+"""min password length"""
 MIN_PASSWORD_LEN = int(environ.get("MIN_PASSWORD_LEN", 5))
-
-# buffer size
-BUFFER_SIZE = int(environ.get("BUFFER_SIZE", 512 * 1024))
-
-# password for encrypt
+"""buffer size"""
+BUFFER_SIZE = environ.get("BUFFER_SIZE", 512 * 1024)
+"""password for encrypt"""
 PASSWORD = str(environ.get("PASSWORD", None))
-
+"""extension for encrypt file"""
+EXTENSION = environ.get("EXTENSION", '.aes')
